f724e9682a56ba0d5d2c2d6c6f36194075422042
Srdjan Babic <srki0012@hotmail.com>
2024-10-19 21:16:31 +0200

    Examples of string concatenation and variable interpolation

---
+++ v2/run_w_options.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+echo "========================================"
+echo "Running perl example1"
+echo
+perl example1
+echo
+echo "========================================"
+echo "Running perl -w example1"
+echo
+perl -w example1
+echo
+echo "========================================"
+echo "Running perl -Mdiagnostics example1"
+echo
+perl -Mdiagnostics example1
+echo
+
+
